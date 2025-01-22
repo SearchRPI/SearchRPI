5957f2e1f45320053633194b83963ff8cdc078ca
Aeonoi <dzhou527@gmail.com>
2025-01-21 22:26:26 -0500

    feat(ui-header): setup header ui

---
+++ ui/src/components/header.tsx
@@ -0,0 +1,13 @@
+const Header: React.FC = () => {
+	return (
+		<nav className="flex justify-between">
+			<p>SearchRPI Logo</p>
+			<div className="flex gap-5">
+				<p>GitHub</p>
+				<p>Dark/Light Mode</p>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
