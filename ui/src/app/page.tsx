5d19a86f9821f9774e3d12a0dac48323f72ad4e5
Aeonoi <dzhou527@gmail.com>
2025-01-24 11:47:50 -0500

    feat(ui-searchbox): add search box that can track the search input

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,11 +1,40 @@
+"use client";
 import Footer from "@/components/footer";
 import Header from "../components/header";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const HomePage: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  // NOTE: Used to test whether searchQuery is correct
+  useEffect(() => {
+    console.log(searchQuery);
+  }, [searchQuery]);
+
+  // NOTE: Used to call backend API to fetch the results
+  const onSubmit = () => {
+    console.log("Search button clicked");
+    console.log(`Search Query: ${searchQuery}`);
+  };
+
   return (
     <div>
       <Header />
-      <div className="h-[90vh]">Hello World</div>
+      <div className="h-[80vh] mt-20">
+        <div className="flex justify-center">
+          {/* TODO: Handle "Submit" or "Enter" key press inside of the Input */}
+          <Input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-[50vw]"
+            placeholder="Search here ..."
+          />
+          <Button onClick={onSubmit}>Search</Button>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
