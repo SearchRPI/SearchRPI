93182b62a2f53369119974f3d0a3042642dfcf1e
Aeonoi <dzhou527@gmail.com>
2025-01-28 17:32:13 -0500

    docs(readme): update readme for features that should be implemented

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -183,6 +183,7 @@ const HomePage: React.FC = () => {
         <div className="flex justify-center items-center h-[50vh] ">
           <div
             className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            // biome-ignore lint/a11y/useSemanticElements: it works
             role="status"
           >
             <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
