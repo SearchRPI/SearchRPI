7314a2b2d4d8720c7b47ebc6c38a791ffe45dfc6
Ethan Balakumar <76022348+etbala@users.noreply.github.com>
2025-01-31 03:17:58 -0500

    Light Ranking Outline

---
+++ include/query.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * @file  query.h
+ * @brief Class for searching a database
+*/
+
+namespace SearchRPI {
+
+class Query {
+
+};
+
+}
---
+++ include/weight.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @file  weight.h
+ * @brief Weighting schemes
+*/
+
+namespace SearchRPI {
+
+// TODO: Ranking Algorithm Here (BM25)
+//       Use class so swap to BM25f (Incorporating Field Weights) will be easier
+
+}
