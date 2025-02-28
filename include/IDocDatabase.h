7b2ccee73d582669352f7df3d78980a6b550c11d
Ethan Balakumar <76022348+etbala@users.noreply.github.com>
2025-02-28 12:51:13 -0500

    Add Doc Database Interface & Mock

--- include/IDocDatabase.h
+++ include/IDocDatabase.h
@@ -1,24 +1,27 @@
 #pragma once
 
+/**
+ * @file  IDocDatabase.h
+ * @brief Interface for database containing document information
+*/
+
 #include "types.h"
 
 #include <string>
 #include <vector>
-
-/*
- *  Document Database
- *  Can find document using either complete URL or Doc ID.
- *  Database is responsible for generating new ID if added
- *  Document does not currently exist.
- */
+#include <set>
 
 /**
- * @brief Interface for Database operations
+ * @brief Interface for Document Databases
  */
 class IDocDatabase {
 public:
     IDocDatabase() = default;
     virtual ~IDocDatabase() = default;
+
+    // Disable Copy Constructor/Assignment Operator
+    IDocDatabase(const IDocDatabase&) = delete;
+    IDocDatabase& operator=(IDocDatabase const&) = delete;
 
     /**
      * @param id Document ID
@@ -30,8 +33,32 @@ public:
      * @param url URL of Site (e.g. sis.rpi.edu) (strip https://)
      * @returns Whether any document is located at that url
      */
-    virtual bool contains(std::string url) const = 0;
+    virtual bool contains(const std::string& url) const = 0;
 
-    virtual SearchRPI::docid updateDoc(std::string url, std::string title, std::string text) const = 0;
+    /**
+     * @param url Page URL
+     * @param title Page Title (Shown in search results)
+     * @param words Text of the page // TODO: Update format to include punctuation & support field weighting
+     * @returns ID Generated for Added Document
+     */
+    virtual SearchRPI::docid addDoc(const std::string& url, const std::string& title, std::vector<std::string> words) = 0;
+
+    /**
+     * @param url Page URL
+     * @returns ID associated with URL
+     */
+    virtual SearchRPI::docid getDocId(const std::string& url) const = 0;
+
+    /**
+     * @param id Document ID
+     * @returns A set of all words in the document
+     */
+    virtual std::set<std::string> getWords(SearchRPI::docid id) const = 0;
+
+    /**
+     * @param id Document ID
+     * @returns Whether document existed and was removed (false if doc didn't exist)
+     */
+    virtual bool remove(SearchRPI::docid id) = 0;
 
 };
