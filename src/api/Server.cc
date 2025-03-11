8c3dc9bf707a92c583b1f745ed8b1e2f80e5658e
Ethan <76022348+etbala@users.noreply.github.com>
2025-03-11 18:21:09 -0400

    RM TEST_MODE

--- src/api/Server.cc
+++ src/api/Server.cc
@@ -2,13 +2,9 @@
 #include "api/Router.h"
 
 namespace SearchRPI {
-    void Server::start(){
-#ifndef TEST_MODE  // Only run in production, not in tests
+    void Server::start() {
         crow::SimpleApp app;
         Router::setupRoutes(app);
         app.port(8080).multithreaded().run();
-#else
-        std::cout << "[TEST_MODE] Server start skipped." << std::endl;
-#endif
     }   
 }
