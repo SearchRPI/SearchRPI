a263d53df79966ff3445623c2514cbf483193738
Amanda Ruan <amandaruan262@gmail.com>
2025-03-22 23:01:05 -0400

    Add logo to header
    Added logo to header and updated it to use the correct logo based on dark/light theme.

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import LightLogo from "./light-logo.png";
+import DarkLogo from "./dark-logo.png";
 import ThemeSwitcher from "./theme-switcher";
 import { Button } from "./ui/button";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useTheme } from "next-themes";
 
 interface Props {
   setSearchQuery: React.Dispatch<React.SetStateAction<string | null>>;
@@ -11,6 +15,10 @@ const Header: React.FC<Props> = ({ setSearchQuery }) => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  // Update logo based on theme
+  const { theme } = useTheme();
+  const logoSrc = theme === "dark" ? DarkLogo : LightLogo
+
   // Remove the search params when clicking on the home page button
   const removeParam = () => {
     const params = new URLSearchParams(searchParams.toString());
@@ -19,15 +27,18 @@ const Header: React.FC<Props> = ({ setSearchQuery }) => {
   };
 
   return (
-    <nav className="flex justify-between p-5">
+    <nav className="flex justify-between items-center p-5">
       {/* TODO: Replace with actual logo as button */}
       <Button
         variant={"ghost"}
-        className="font-bold text-2xl text-left"
+        className="font-bold text-2xl text-left pt-8 px-0"
         asChild
         onClick={removeParam}
+        
       >
-        <Link href={"/"}>SearchRPI Logo</Link>
+        <Link href={"/"}>
+          <Image src={logoSrc} alt="SearchRPI Logo" width={240} height={240}/>
+        </Link>
       </Button>
       <ThemeSwitcher />
     </nav>
