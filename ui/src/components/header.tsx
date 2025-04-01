54af99ecc66cbbfe950365d76b1e3210ecf14530
Amanda Ruan <amandaruan262@gmail.com>
2025-04-01 17:00:05 -0400

    Fix logo and remove pagination on landing page
    Fixed error where logo does not use correct image when theme switches. Removed pagination from the landing page and set it to only appear when showing results.

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -12,9 +12,10 @@ interface Props {
   searchQuery: string;
   setSearchQuery: React.Dispatch<React.SetStateAction<string | null>>;
   onSubmit: () => void;
+  minimal: boolean;
 }
 
-const Header: React.FC<Props> = ({ searchQuery, setSearchQuery, onSubmit }) => {
+const Header: React.FC<Props> = ({ searchQuery, setSearchQuery, onSubmit , minimal }) => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -30,30 +31,41 @@ const Header: React.FC<Props> = ({ searchQuery, setSearchQuery, onSubmit }) => {
   };
 
   return (
-    <nav className="flex justify-between items-center p-5">
-      {/* TODO: Replace with actual logo as button */}
-      <Button
-        variant={"ghost"}
-        className="font-bold text-2xl text-left pt-8 px-0"
-        asChild
-        onClick={removeParam}
-      >
-        <Link href={"/"}>
-          <Image src={logoSrc} alt="SearchRPI Logo" width={240} height={240}/>
-        </Link>
-      </Button>
-      <div className="flex justify-center">
-        {/* TODO: Handle "Submit" or "Enter" key press inside of the Input */}
-        <Input
-          value={searchQuery || ""}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-[40vw]"
-          placeholder="Search here ..."
-          id="SearchBox"
-        />
-      </div>
-      <ThemeSwitcher/>
-    </nav>
+    <div>
+      {!minimal ? (
+        <nav className="flex justify-between items-center p-5">
+          {/* TODO: Replace with actual logo as button */}
+          <Button
+            variant={"ghost"}
+            className="font-bold text-2xl text-left pt-8 px-0"
+            asChild
+            onClick={removeParam}
+          >
+            <Link href={"/"}>
+              <Image src={logoSrc} alt="SearchRPI Logo" width={240} height={240}/>
+            </Link>
+          </Button>
+          <div className="flex justify-center">
+            {/* TODO: Handle "Submit" or "Enter" key press inside of the Input */}
+            <Input
+              value={searchQuery || ""}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="w-[40vw]"
+              placeholder="Search here ..."
+              id="SearchBox"
+            />
+          </div>
+          <ThemeSwitcher/>
+        </nav>
+      ):(
+        <nav className="flex justify-end items-center p-5">
+          <ThemeSwitcher/>
+        </nav>
+      )}
+    </div>
+    // update header so that theme changer appears on landing page and entire header appears on search results page
+
+
   );
 };
 
