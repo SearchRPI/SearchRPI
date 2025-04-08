ed4d91b5e6cf63c87376a80662269a4364af8f80
Arct1cPharaoh <Arct1cPharaoh@users.noreply.github.com>
2025-04-08 21:27:12 +0000

    deploy: fee2ea2c8f94d013af68db505d2c1e941670d43e

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['t_0',['t',['../classcrow_1_1json_1_1rvalue.html#a9c9f7242e4164f4ed690e50e3cf5f4b1',1,'crow::json::rvalue::t()'],['../classcrow_1_1json_1_1wvalue.html#a2e8d3ed2849f16bb1894c4d48cb49921',1,'crow::json::wvalue::t()']]],
+  ['t_0',['t',['../classcrow_1_1json_1_1wvalue.html#a2e8d3ed2849f16bb1894c4d48cb49921',1,'crow::json::wvalue::t()'],['../classcrow_1_1json_1_1rvalue.html#a9c9f7242e4164f4ed690e50e3cf5f4b1',1,'crow::json::rvalue::t()']]],
   ['taggedrule_1',['TaggedRule',['../classcrow_1_1TaggedRule.html#a058a079101cee2bcc5936c785c6c76f6',1,'crow::TaggedRule']]],
   ['task_5ftimer_2',['task_timer',['../classcrow_1_1detail_1_1task__timer.html#a82b70debe9a0cd23cb42fa8122d6d984',1,'crow::detail::task_timer']]],
   ['template_5ft_3',['template_t',['../classcrow_1_1mustache_1_1template__t.html#a8c37d92fba8f0a4cf4d4f7a254847ccc',1,'crow::mustache::template_t']]],
-  ['terms_4',['terms',['../classRanking_1_1Query.html#a750ae2b932bda721152c096b831e185a',1,'Ranking::Query']]],
-  ['tfidfweight_5',['TFIDFWeight',['../classRanking_1_1TFIDFWeight.html#a114985f54463ef7851eff480da03b97b',1,'Ranking::TFIDFWeight']]],
-  ['tick_6',['tick',['../classcrow_1_1Crow.html#a7d5baf439ee8c85ad500cdb022bcd3a1',1,'crow::Crow']]],
-  ['timeout_7',['timeout',['../classcrow_1_1Crow.html#a1d69f60255f2bf1571d6570a6abb1552',1,'crow::Crow']]],
-  ['tokenize_8',['tokenize',['../query-processing_2query_8cc.html#a76809aec2d62960dd863b51fc64240ff',1,'tokenize():&#160;query.cc'],['../namespacequery.html#a98ca2986b1041fb85ed265f3ff140f77',1,'query::tokenize()']]],
-  ['tolower_9',['toLower',['../query-processing_2query_8cc.html#a2daf73440b231c35077478fe95c4b4ff',1,'toLower():&#160;query.cc'],['../namespacequery.html#a93332adaefdef9bf011dc6839eabd41b',1,'query::toLower()']]],
-  ['tostring_10',['toString',['../namespacequeryTree.html#aa36b7f6c51516533922d625d57b935a4',1,'queryTree']]],
-  ['trie_11',['Trie',['../classcrow_1_1Trie.html#ae6b070e1e25b6c5d77cfb2aa05473d73',1,'crow::Trie']]]
+  ['termdoccount_4',['termDocCount',['../classDatabase.html#a8e930a8b04840ac97480a8cfce65381e',1,'Database::termDocCount()'],['../classIDatabase.html#a5c91b85ea88669a454dc4f663befb566',1,'IDatabase::termDocCount()']]],
+  ['terms_5',['terms',['../classRanking_1_1Query.html#a750ae2b932bda721152c096b831e185a',1,'Ranking::Query']]],
+  ['tfidfweight_6',['TFIDFWeight',['../classRanking_1_1TFIDFWeight.html#a114985f54463ef7851eff480da03b97b',1,'Ranking::TFIDFWeight']]],
+  ['tick_7',['tick',['../classcrow_1_1Crow.html#a7d5baf439ee8c85ad500cdb022bcd3a1',1,'crow::Crow']]],
+  ['timeout_8',['timeout',['../classcrow_1_1Crow.html#a1d69f60255f2bf1571d6570a6abb1552',1,'crow::Crow']]],
+  ['tokenize_9',['tokenize',['../query-processing_2query_8cc.html#a76809aec2d62960dd863b51fc64240ff',1,'tokenize():&#160;query.cc'],['../namespacequery.html#a98ca2986b1041fb85ed265f3ff140f77',1,'query::tokenize()']]],
+  ['tolower_10',['toLower',['../query-processing_2query_8cc.html#a2daf73440b231c35077478fe95c4b4ff',1,'toLower():&#160;query.cc'],['../namespacequery.html#a93332adaefdef9bf011dc6839eabd41b',1,'query::toLower()']]],
+  ['tostring_11',['toString',['../namespacequeryTree.html#aa36b7f6c51516533922d625d57b935a4',1,'queryTree']]],
+  ['trie_12',['Trie',['../classcrow_1_1Trie.html#ae6b070e1e25b6c5d77cfb2aa05473d73',1,'crow::Trie']]]
 ];
